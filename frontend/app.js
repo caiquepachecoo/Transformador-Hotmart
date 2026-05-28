41ee6d69cec72c31ae9e904cc3691c004bc4fc7e
caiquepachecoo <caique.pacheco@hotmart.com>
2026-05-28 16:41:48 -0300

    Implement PDF upload and result display functionality
    Add async converter function to handle PDF uploads and display results

--- frontend/app.js
+++ frontend/app.js
@@ -1,1 +1,38 @@
+async function converter(){
 
+const arquivo =
+document.getElementById("pdf").files[0];
+
+if(!arquivo){
+alert("Escolha um PDF");
+return;
+}
+
+const formData = new FormData();
+
+formData.append("pdf", arquivo);
+
+const req = await fetch(
+"https://transformador-hotmart.onrender.com/upload",
+{
+method:"POST",
+body:formData
+}
+);
+
+const data = await req.json();
+
+document.getElementById(
+"resultado"
+).innerHTML = `
+<h2>URL</h2>
+<input value="${data.url}" readonly>
+
+<h2>Iframe</h2>
+
+<textarea readonly>
+${data.iframe}
+</textarea>
+`;
+
+}
